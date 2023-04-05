04540765c0dd4c72ed937dcdcb2a545f68c41bb6
acdevs <amanchandra.in@gmail.com>
2023-04-05 22:21:36 +0530

    callbacks.

--- callbacks.js
+++ callbacks.js
@@ -1,1 +1,24 @@
-/* js is synchronus and single threaded language! */
+/* js is synchronus and single threaded language! 
+"Higher order functions are functions that take other functions as arguments : callbacks!"
+welcome to asynchronous world in synchronous language! */
+
+// setTimeout(function(){
+//     console.log("Hi!");
+// }, 5000);
+
+// function x(y){
+//     console.log("x");
+//     y()
+// }
+
+// x(function(){
+//     console.log("y");
+// });
+
+// function attachEventListener(){
+//     let count = 0;
+//     document.getElementById("btn").addEventListener("click", function(){
+//         console.log("clicked", ++count, "times!");
+//     });
+// }
+// attachEventListener();
