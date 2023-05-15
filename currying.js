0c49d49551da7ad3c7244654a65eb9015d5b3f8f
acdevs <amanchandra.in@gmail.com>
2023-05-15 14:51:59 +0530

    memo

--- currying.js
+++ currying.js
@@ -5,7 +5,6 @@ functions, each taking a single argument.
 What is it for? 
     Currying helps you avoid passing the same variable multiple times */
 
-
 /* using bind()... */
 
 // let multiply = function(a, b) {
@@ -19,12 +18,38 @@ What is it for?
 
 /* using closures... */
 
-function multiply(a) {
-    return function(b) {
-        console.log(a * b);
-    }
-}
-let multiplyBy2 = multiply(2);
-multiplyBy2(5);
-let multiplyBy3 = multiply(3);
-multiplyBy3(5);
+// function multiply(a) {
+//     return function(b) {
+//         console.log(a * b);
+//     }
+// }
+// let multiplyBy2 = multiply(2);
+// multiplyBy2(5);
+// let multiplyBy3 = multiply(3);
+// multiplyBy3(5);
+
+
+/* Use Cases! */
+// Reusable utility functions:
+const add = a => b => a + b;
+const add2 = add(2);
+console.log(add2(5));
+
+// Event handling: In event-driven programming, event handlers with specific configurations,
+
+var curry = function(fn) {
+    return function curried(...args) {
+      if (args.length >= fn.length) {
+        return fn.apply(this, args);
+      }
+  
+      return curried.bind(this, ...args);
+    };
+  };
+  
+  /**
+   * function sum(a, b) { return a + b; }
+   * const csum = curry(sum);
+   * csum(1)(2) // 3
+   */
+  
--- memoize.js
+++ memoize.js
@@ -4,4 +4,74 @@ Instead it will return a cached value. Memoization only works correctly for pure
 A pure function is defined as function that always returns the same output 
 given the same inputs and doesn't have any side-effects. */
 
+/* Memoization in Web D:
+    Caching website files,
+    caching API calls, MOREEE on it later!
+    unchanged react components */
 
+/* A potential downside of memoizing network requests is the risk of data staleness. */
+
+/* Memoization in JS:
+    1. Using a cache object
+    2. Using a Map object
+    3. Using a WeakMap object */
+
+// 1. Using a cache object
+// const memo = (fn) => {
+//     const cache = {};
+//     return (...args) => {
+//         let key = '';
+//         for (const arg of args) {
+//             key += arg + '.';
+//         }
+//         if (key in cache) {
+//             console.log('👀');
+//             return cache[key];
+//         }
+//         cache[key] = fn(...args);
+//         return cache[key];
+//     }
+// }
+
+
+// 2. Using a Map object
+// const memo = (fn) => {
+//     const cache = new Map(); 
+//     return (...args) => {
+//         let key = JSON.stringify(args);
+//         if (cache.has(key)) {
+//             console.log('👀');
+//             return cache.get(key);
+//         }
+//         cache.set(key, fn(...args));
+//         return cache.get(key);
+//     }
+// }
+
+
+
+// 3. Using a WeakMap object
+// const memo = (fn) => {
+//     const cache = new WeakMap(); 
+//     return (...args) => {
+//         let key = JSON.stringify(args);
+//         if (cache.has(key)) {
+//             console.log('👀');
+//             return cache.get(key);
+//         }
+//         cache.set(key, fn(...args));
+//         return cache.get(key);
+//     }
+// }
+
+
+//4. One liner
+const memo = (fn, cache = {}) => (...args) => cache[args.join()] ?? (cache[args.join()] = fn(...args));
+
+
+function addTo80(n) {
+    return n + 80;
+}
+const memoizedAddTo80 = memo(addTo80);
+console.log('1', memoizedAddTo80(5));
+console.log('2', memoizedAddTo80(5));
