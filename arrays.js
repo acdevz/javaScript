a6bb8b22402bd0087c358bbe38fe0b9867955e1e
Astha Verma <astha24verma@gmail.com>
2024-06-25 16:54:14 +0530

    modify arrays.js

--- arrays.js
+++ arrays.js
@@ -2,9 +2,11 @@ const arr = [1, 2, 3, 4, 5];
 
 // console.log(arr.length);
 
-arr.push(6);
+arr.push(6); // add element to the end
 
 console.log(arr.toString());
+
+arr.pop(); // remove element from the end
 
 arr.shift(); // remove first element
 arr.unshift(0); // add element to the beginning
